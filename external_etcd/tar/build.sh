86f1fc38bebdbe03bff9a88691f0b6c74425a321
Duncan Fermor <duncan@elitemail.org>
2016-04-21 22:10:41 +0100

    stage 1.5

--- external_etcd/tar/build.sh
+++ external_etcd/tar/build.sh
@@ -7,13 +7,15 @@ bundle exec ruby generate.rb
 
 rm -rf dist
 mkdir -p dist
-tar --create --file=dist/primary.tar   payload/primary
-tar --create --file=dist/secondary.tar payload/secondary
-tar -tvf dist/primary.tar
-tar -tvf dist/secondary.tar
+tar --create --file=dist/192.168.33.10.tar payload/192.168.33.10
+tar --create --file=dist/192.168.33.11.tar payload/192.168.33.11
+tar --create --file=dist/192.168.33.12.tar payload/192.168.33.12
+tar -tvf dist/192.168.33.10.tar
+tar -tvf dist/192.168.33.11.tar
+tar -tvf dist/192.168.33.12.tar
 
-scp dist/primary.tar vagrant@192.168.33.10:payload.tar
-scp dist/secondary.tar vagrant@192.168.33.11:payload.tar
-scp dist/secondary.tar vagrant@192.168.33.12:payload.tar
+scp dist/192.168.33.10.tar vagrant@192.168.33.10:payload.tar
+scp dist/192.168.33.11.tar vagrant@192.168.33.11:payload.tar
+scp dist/192.168.33.12.tar vagrant@192.168.33.12:payload.tar
 
-sup vagrant deploy
+sup vagrant deploy_stage_1 deploy_stage_2 deploy_stage_3 deploy_stage_4
--- external_etcd/tar/generate.rb
+++ external_etcd/tar/generate.rb
@@ -1,6 +1,12 @@
 require 'erubis'
 require 'fileutils'
 
+hosts = [
+  '192.168.33.10', 
+  '192.168.33.11', 
+  '192.168.33.12'
+]
+primary = '192.168.33.10'
 virtual_ip = '192.168.33.100'
 interface = 'enp0s8'
 
@@ -27,26 +33,68 @@ build_config(
     virtual_ip: virtual_ip,
     interface: interface
   },
-  'payload/primary/etc/keepalived',
+  "payload/#{primary}/etc/keepalived",
   'keepalived.conf'
 )
 
-build_config(
-  'templates/keepalived.conf.eruby', {
-    priority: 100,
-    role: 'BACKUP',
-    virtual_ip: virtual_ip,
-    interface: interface
-  },
-  'payload/secondary/etc/keepalived',
-  'keepalived.conf'
-)
+hosts.each do |host|
+  next host if host == primary
+  build_config(
+    'templates/keepalived.conf.eruby', {
+      priority: 100,
+      role: 'BACKUP',
+      virtual_ip: virtual_ip,
+      interface: interface
+    },
+    "payload/#{host}/etc/keepalived",
+    'keepalived.conf'
+  )
+end
 
-['primary', 'secondary'].each do |type|
+hosts.each do |host|
+  type = host == primary ? 'primary' : 'secondary'
   build_config(
     'templates/keepalived_systemd_override.conf', 
     {},
     "payload/#{type}/etc/systemd/system/keepalived.conf.d",
     'override.conf'
+  )
+end
+
+hosts.each do |host|
+  build_config(
+    'templates/kubelet_config', 
+    { 
+			gateway_ip: virtual_ip,
+      ip: host
+    },
+    "payload/#{host}/etc/kubernetes",
+    'kubelet'
+  )
+end
+
+hosts.each do |host|
+  build_config(
+    'templates/haproxy_config', 
+    {},
+    "payload/#{host}/etc/haproxy",
+    'haproxy.cfg'
+  )
+end
+
+hosts.each_with_index do |host, index|
+  build_config(
+    'templates/etcd.conf', 
+    {
+      index: index,
+      cluster_name: "2016-04-21-cumulo-etcd",
+      initial_cluster: hosts
+        .each_with_index
+        .map { |h, i| "etcd#{i}=http://#{h}:2380" }
+        .join(","),
+      ip: host
+    },
+    "payload/#{host}/etc/etcd",
+    'etcd.conf'
   )
 end
